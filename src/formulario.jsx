2887d3fa2357b08b67f87332de68011b6b3bf95a
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2024-02-12 17:02:09 -0300

    pegando a informcao do campo input e jogando no cadastro do backend

--- src/formulario.jsx
+++ src/formulario.jsx
@@ -1,4 +1,4 @@
-function Formulario ({botao,eventoTeclado}){
+function Formulario ({botao,eventoTeclado, cadastrar}){
     return (
         
         <form>
@@ -8,7 +8,7 @@ function Formulario ({botao,eventoTeclado}){
             {
                 botao
                 ?
-                <input type="button" value="Cadastrar" className="btn-primary"></input>
+                <input type="button" onClick={cadastrar} value="Cadastrar" className="btn-primary"></input>
                 :
                 <div>
                     <input type="button" value="Alterar"className="btn-warning"></input>
